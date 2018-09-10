ad13cb78efe4bf08230af2d946bd82f5e9209209
Neil Agarwal <regalstreak@gmail.com>
2018-09-11 01:36:27 +0530

    transfer: Get frontend ready

---
+++ src/components/HomeView/HomeView.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-container fluid>
+    <v-slide-y-transition mode="out-in">
+      <v-layout column>
+
+        <div >
+            <v-btn round color="primary" dark>Upload</v-btn>
+        </div>
+
+        <space></space>
+
+        <div>
+            <v-list light>
+
+                <v-list-tile
+                    v-for="item in files"
+                    :key="item.title"
+                    avatar
+                    @click="console.log(s)"
+                >
+                    <v-list-tile-avatar>
+                    <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                    </v-list-tile-avatar>
+
+                    <v-list-tile-content>
+                    <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    <v-list-tile-sub-title>{{ item.size }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+
+                    <v-list-tile-action>
+                    <v-btn icon ripple>
+                        <v-icon color="grey lighten-1">info</v-icon>
+                    </v-btn>
+                    </v-list-tile-action>
+                </v-list-tile>
+            </v-list>
+        </div>
+        
+
+      </v-layout>
+    </v-slide-y-transition>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      files: [
+        {
+          icon: "assignment",
+          iconClass: "blue white--text",
+          title: "Vacation itinerary",
+          size: "5kb"
+        },
+        {
+          icon: "call_to_action",
+          iconClass: "amber white--text",
+          title: "Kitchen remodel",
+          size: "10kb"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+space {
+  margin-top: 20px;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,15 @@
 import Vue from "vue";
 import Router from "vue-router";
+import HomeView from "./components/HomeView/HomeView.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  routes: []
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView
+    }
+  ]
 });
