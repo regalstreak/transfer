13afd2a99db19f296be21ac662872ee3d6ac6e32
Neil Agarwal <regalstreak@gmail.com>
2018-09-15 18:02:10 +0530

    transfer: Hide duplicates

--- src/components/HomeView/FolderView.vue
+++ src/components/HomeView/FolderView.vue
@@ -11,12 +11,13 @@
                     :key="index"
                     avatar
                     @click="download(item)"
+                    v-if="item.fileSize != 0 && item.fileUrl != ''"
                 >
                     <v-list-tile-avatar>
                     <v-icon class="blue white--text">assignment</v-icon>
                     </v-list-tile-avatar>
 
-                    <v-list-tile-content>
+                    <v-list-tile-content >
                     <v-list-tile-title>{{ item.fileName }}</v-list-tile-title>
                     <v-list-tile-sub-title>{{ item.fileSize }}</v-list-tile-sub-title>
                     </v-list-tile-content>
